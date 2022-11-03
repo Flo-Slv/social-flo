32fa0c9db26f4a931f8491f5408e416fe714e96d
Flo Slv <florent3969@hotmail.fr>
2022-11-03 22:35:12 +0100

    🟦 feat: login page

---
+++ app/(login)/page.jsx
@@ -0,0 +1,29 @@
+import './login.scss';
+
+const Login = () => {
+	return (
+		<div className={'login'}>
+			<h1>Social Flo</h1>
+
+			<form>
+				<input
+					type='email'
+					name='email'
+					placeholder='Email'
+					required
+				/>
+
+				<input
+					type='password'
+					name='password'
+					placeholder='Password'
+					minLength='10'
+					required
+				/>
+				<button className={'login-button'}>Log in</button>
+			</form>
+		</div>
+	)
+};
+
+export default Login;
