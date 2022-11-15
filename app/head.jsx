fc776c282b515f30f697c2133012dfa708006036
Flo Slv <florent3969@hotmail.fr>
2022-11-15 14:24:14 +0100

    🟦 feat: add Head feature in a separate file

---
+++ app/head.jsx
@@ -0,0 +1,37 @@
+const Head = () => {
+	return (
+		<>
+			<meta
+				name='description'
+				content='Social Flo'
+			/>
+
+			<meta
+				name='viewport'
+				content='width=device-width, initial-scale=1.0'
+			/>
+
+			<link
+				rel='stylesheet'
+				href='https://fonts.googleapis.com/css?family=Lobster'
+				precedence='default'
+			/>
+
+			<link
+				rel='stylesheet'
+				href='https://fonts.cdnfonts.com/css/gotham-rounded'
+				precedence='default'
+			/>
+
+			<link
+				rel='icon'
+				href='/favicon.ico'
+				precedence='default'
+			/>
+
+			<title>Social Flo</title>
+		</>
+	);
+};
+
+export default Head;
--- app/layout.jsx
+++ app/layout.jsx
@@ -5,31 +5,7 @@ import './global.scss';
 const RootLayout = ({ children }) => {
 	return (
 		<html lang='en'>
-			<head>
-				<meta
-					name='description'
-					content='Social Flo'
-				/>
-
-				<meta
-					name='viewport'
-					content='width=device-width, initial-scale=1.0'
-				/>
-
-				<link
-					rel='stylesheet'
-					href='https://fonts.googleapis.com/css?family=Lobster'
-				/>
-
-				<link
-					rel='stylesheet'
-					href='https://fonts.cdnfonts.com/css/gotham-rounded'
-				/>
-
-				<link rel='icon' href='/favicon.ico' />
-
-				<title>Social Flo</title>
-			</head>
+			<head />
 
 			<body>
 				<ThemeProvider>
