7f57f115f3fa2822072a0f0a5ab2eaa6e019dd32
Flo Slv <florent3969@hotmail.fr>
2022-11-23 16:10:22 +0100

    🟦 feat: improve fetching functions

--- prisma/utils/users.js
+++ prisma/utils/users.js
@@ -10,23 +10,50 @@ export const getUsers = async () => {
 	}
 };
 
-export const createUser = async user => {
+export const getUserById = async id => {
 	try {
-		const userFromDB = await prisma.user.create({
-			data: user
-		});
-
-		return { user: userFromDB };
+		const user = await prisma.user.findUnique({ where: { id } });
+		return { user };
 	}
 	catch (error) {
 		return { error };
 	}
 };
 
-export const getUserById = async id => {
+export const createUser = async user => {
 	try {
-		const user = await prisma.user.findUnique({ where: { id } });
-		return { user };
+		const existingUser = await prisma.user.findUnique({
+			where: { email: user.email }
+		});
+
+		if (existingUser) return { error: 'Email already taken !' }
+
+		const newUser = await prisma.user.create({
+			data: user
+		});
+
+		return { user: newUser };
+	}
+	catch (error) {
+		return { error };
+	}
+};
+
+export const login = async user => {
+	try {
+		const existingUser = await prisma.user.findUniqueOrThrow({
+			where: { email: user.email }
+		});
+
+		if (existingUser) {
+			console.log('login existingUser: ', existingUser);
+
+			if (existingUser.password !== user.password.trim()) {
+				return { error: 'Bad password !' };
+			}
+
+			return { user: existingUser };
+		}
 	}
 	catch (error) {
 		return { error };
