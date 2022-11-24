86674e8db1e413e40c47d423102261425b799c19
Flo Slv <florent3969@hotmail.fr>
2022-11-24 09:24:53 +0100

    🟦 feat: add checking when create account or login

--- prisma/utils/users.js
+++ prisma/utils/users.js
@@ -24,14 +24,32 @@ export const getUserById = async id => {
 
 export const createUser = async user => {
 	try {
+		const email = user.email.trim();
+		const password = user.password.trim();
+		const confirm_password = user.confirm_password.trim();
+
+		// Check if email are empty.
+		if (email === '') return { error: 'Email must not be empty !' };
+
+		// Check if email is valid.
+		const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+
+		if (!email.match(regEx)) return { error: 'Email is not valid' };
+
+		// Check if user already exist.
 		const existingUser = await prisma.user.findUnique({
-			where: { email: user.email.trim() }
+			where: { email: email }
 		});
 
 		if (existingUser) return { error: 'Email already taken !' };
 
-		if (user.password.trim() !== user.confirm_password.trim())
-			return { error: 'Both password should be identical !' };
+		// Check if passwords are empty.
+		if (password === '' || confirm_password === '')
+			return { error: 'Passwords must not be empty !' };
+
+		// Check if password are identical.
+		if (password !== confirm_password)
+			return { error: 'Passwords should be identical !' };
 
 		// Do not send 'confirm_password' to DB.
 		delete user.confirm_password;
@@ -39,9 +57,8 @@ export const createUser = async user => {
 		// Hash the password.
 		user.password = bcrypt.hashSync(user.password, 10);
 
-		const newUser = await prisma.user.create({
-			data: user
-		});
+		// Create user in DB.
+		const newUser = await prisma.user.create({ data: user });
 
 		return { user: newUser };
 	}
@@ -52,8 +69,15 @@ export const createUser = async user => {
 
 export const login = async user => {
 	try {
+		const email = user.email.trim();
+		const password = user.password.trim();
+
+		// Check if email and password are not empty.
+		if (email === '' || password === '')
+			return { error: 'Fieds can not be empty !' };
+
 		const existingUser = await prisma.user.findUniqueOrThrow({
-			where: { email: user.email }
+			where: { email: email }
 		});
 
 		if (existingUser) {
