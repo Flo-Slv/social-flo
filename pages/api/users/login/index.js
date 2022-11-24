71075199b7dccc71c0d673be49fa46200fc5efc6
Flo Slv <florent3969@hotmail.fr>
2022-11-24 15:44:01 +0100

    🟦 feat: add jwt token + cookie when login

--- pages/api/users/login/index.js
+++ pages/api/users/login/index.js
@@ -1,4 +1,7 @@
-import { login } from '/prisma/utils/users.js';
+import { sign } from 'jsonwebtoken';
+import { serialize } from 'cookie';
+
+import { login } from '../../../../prisma/utils/users.js';
 
 const handler = async (req, res) => {
 	if (req.method === 'POST') {
@@ -9,10 +12,28 @@ const handler = async (req, res) => {
 
 			if (error) throw new Error(error);
 
-			return res.status(200).json({ 
-				message: 'User successfully login !',
-				user
+			const secret = process.env.SECRET_KEY;
+
+			const token = sign(
+				{
+					exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30, // 30 days
+					id: user.id,
+					email: user.email
+				},
+				secret
+			);
+
+			const cookie = serialize('SocialFloJWT', token, {
+				httpOnly: Boolean(true),
+				secure: process.env.NODE_ENV !== 'development',
+				sameSite: 'strict',
+				maxAge: 60 * 60 * 24 * 30,
+				path: '/'
 			});
+
+			res.setHeader('Set-Cookie', cookie);
+
+			return res.status(200).json({ message: 'User successfully login !' });
 		}
 		catch (error) {
 			return res.status(500).json({ error: error.message });
