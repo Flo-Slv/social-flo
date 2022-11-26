86b1a094beb0682485100b6843c27d7be01ac529
Flo Slv <florent3969@hotmail.fr>
2022-11-26 13:32:08 +0100

    🟪 refactor: remove AuthContext since we don't use it

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from '../context/theme/theme-provider.jsx';
-import { AuthProvider } from '../context/theme/auth-provider.jsx';
 
 import '/styles/global.scss';
 
@@ -9,11 +8,9 @@ const RootLayout = ({ children }) => {
 			<head />
 
 			<body>
-				<AuthProvider>
-					<ThemeProvider>
-						{children}
-					</ThemeProvider>
-				</AuthProvider>
+				<ThemeProvider>
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
