ea98594b3d5a56916378bc87988d7f9135c67cd6
Flo Slv <florent3969@hotmail.fr>
2022-12-17 12:57:12 +0100

    🟦 feat: add options into next.config.mjs for Sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
 	experimental: {
 		appDir: true
+	},
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import '_colors.scss';`
 	}
 };
 
