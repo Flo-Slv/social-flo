af86c3f4bb793f7d2dabad1b49dd595ff1c76e4e
Flo Slv <florent3969@hotmail.fr>
2022-12-26 11:02:34 +0100

    🟪 refactor: backend profile - prettier

--- app/(backend)/profile/page.jsx
+++ app/(backend)/profile/page.jsx
@@ -1,25 +1,21 @@
-'use client';
+"use client";
 
-import { useContext } from 'react';
-import { CurrentUserContext } from '../../../context/current-user/current-user-provider.jsx';
+import { useContext } from "react";
+import { CurrentUserContext } from "../../../context/current-user/current-user-provider.jsx";
 
-import styles from '../../../styles/backend/profile.module.scss';
+import styles from "../../../styles/backend/profile.module.scss";
 
 const Profile = () => {
-	const { currentUser } = useContext(CurrentUserContext);
-	console.log('profile currentUser: ', currentUser);
+  const { currentUser } = useContext(CurrentUserContext);
+  console.log("profile currentUser: ", currentUser);
 
-	return (
-		<div className={styles.profile}>
-			<div>
-				Name: {currentUser.name || 'no name yet...'} 
-			</div>
+  return (
+    <div className={styles.profile}>
+      <div>Name: {currentUser.name || "no name yet..."}</div>
 
-			<div>
-				Email: {currentUser.email}
-			</div>
-		</div>
-	);
+      <div>Email: {currentUser.email}</div>
+    </div>
+  );
 };
 
 export default Profile;
