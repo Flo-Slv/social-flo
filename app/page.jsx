c3099236d4eb96ee4ec3ac405e51544db0b39b1b
Flo Slv <florent3969@hotmail.fr>
2022-12-28 23:12:27 +0100

    🟪 refactor: remove unsuse user context in homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,4 @@
-"use client";
-
-import { useContext } from "react";
-import { CurrentUserContext } from "../context/current-user/current-user-provider.jsx";
-
 const Home = () => {
-  const { currentUser } = useContext(CurrentUserContext);
-  console.log("homepage currentUser: ", currentUser);
-
   return <div className={"underline"}>Homepage</div>;
 };
 
