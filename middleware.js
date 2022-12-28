3fb11b4057091b1c1268827199836c24ce99c76c
Flo Slv <florent3969@hotmail.fr>
2022-12-28 22:16:57 +0100

    🟪 refactor: middleware prettier

--- middleware.js
+++ middleware.js
@@ -1,24 +1,32 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-import verifyJwt from './utils/jose/verify-jwt.js';
+import verifyJwt from "./utils/jose/verify-jwt.js";
 
-const publicRoutes = ['/', '/sign-in', '/sign-up', '/football', '/rugby', '/judo'];
-const protectedRoutes = ['/profile', '/sports', '/todo'];
+const publicRoutes = [
+  "/",
+  "/sign-in",
+  "/sign-up",
+  "/football",
+  "/rugby",
+  "/judo",
+];
+const protectedRoutes = ["/profile", "/sports", "/todo"];
 
-export const middleware = async request => {
-	const hasJWT = request.cookies.has('currentUser');
+export const middleware = async (request) => {
+  const hasJWT = request.cookies.has("currentUser");
 
-	// Redirect to login page if not logged.
-	if (protectedRoutes.includes(request.nextUrl.pathname) && !hasJWT)
-		return NextResponse.redirect(new URL('/sign-in', request.url));
+  // Redirect to login page if not logged.
+  if (protectedRoutes.includes(request.nextUrl.pathname) && !hasJWT)
+    return NextResponse.redirect(new URL("/sign-in", request.url));
 
-	// Redirect to homepage if already logged.
-	if (publicRoutes.includes(request.nextUrl.pathname) && hasJWT) {
-		// Get jwt.
-		const jwt = request.cookies.get('currentUser')?.value;
+  // Redirect to profile if already logged.
+  if (publicRoutes.includes(request.nextUrl.pathname) && hasJWT) {
+    // Get jwt.
+    const jwt = request.cookies.get("currentUser")?.value;
 
-		const isVerified = await verifyJwt(jwt, 'middleware', NextResponse);
+    const isVerified = await verifyJwt(jwt, "middleware", NextResponse);
 
-		if (isVerified) return NextResponse.redirect(new URL('/profile', request.url));
-	}
+    if (isVerified)
+      return NextResponse.redirect(new URL("/profile", request.url));
+  }
 };
