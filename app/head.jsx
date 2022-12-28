6539803589b52a3de6bb2b1eaf24b9657785a09c
Flo Slv <florent3969@hotmail.fr>
2022-12-28 10:20:38 +0100

    🟪 refactor: prettier

--- app/head.jsx
+++ app/head.jsx
@@ -1,43 +1,33 @@
 const Head = () => {
-	return (
-		<>
-			<meta
-				name='description'
-				content='Social Flo'
-			/>
+  return (
+    <>
+      <meta name="description" content="Social Flo" />
 
-			<meta
-				name='viewport'
-				content='width=device-width, initial-scale=1.0'
-			/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-			<link
-				rel='stylesheet'
-				href='https://fonts.googleapis.com/css?family=Lobster'
-				precedence='default'
-			/>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Lobster"
+        precedence="default"
+      />
 
-			<link
-				rel='stylesheet'
-				href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
-				precedence='default'
-			/>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        precedence="default"
+      />
 
-			<link
-				rel='stylesheet'
-				href='https://fonts.cdnfonts.com/css/gotham-rounded'
-				precedence='default'
-			/>
+      <link
+        rel="stylesheet"
+        href="https://fonts.cdnfonts.com/css/gotham-rounded"
+        precedence="default"
+      />
 
-			<link
-				rel='icon'
-				href='/favicon.ico'
-				precedence='default'
-			/>
+      <link rel="icon" href="/favicon.ico" precedence="default" />
 
-			<title>Social Flo</title>
-		</>
-	);
+      <title>Social Flo</title>
+    </>
+  );
 };
 
 export default Head;
