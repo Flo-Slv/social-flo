dfe3f19d82bb2d94f256b8229c3523fec33b3c44
Flo Slv <florent3969@hotmail.fr>
2023-01-02 16:34:33 +0100

    🟪 refactor: prettier

--- pages/api/users/delete-user/index.js
+++ pages/api/users/delete-user/index.js
@@ -1,31 +1,29 @@
-import { removeUserById } from '../../../../prisma/utils/users.js';
+import { removeUserById } from "../../../../prisma/utils/users.js";
 
-import isUserLogged from '../../../../utils/jose/is-user-logged.js';
+import isUserLogged from "../../../../utils/jose/is-user-logged.js";
 
 const handler = async (req, res) => {
-	if (!isUserLogged(req, res)) {
-		return res.status(500).json({ error: 'Invalid token !' });
-	}
+  if (!isUserLogged(req, res)) {
+    return res.status(500).json({ error: "Invalid token !" });
+  }
 
-	if (req.method === 'DELETE') {
-		try {
-			const { user, error } = await removeUserById(req.query.id);
+  if (req.method === "DELETE") {
+    try {
+      const { user, error } = await removeUserById(req.query.id);
 
-			if (error) throw new Error(error);
+      if (error) throw new Error(error);
 
-			return res.status(200).json({
-				message: 'User successfully deleted !',
-				user
-			});
-		}
-		catch (error) {
-			return res.status(500).json({ error: error.message });
-		}
-	}
+      return res.status(200).json({
+        message: "User successfully deleted !",
+        user,
+      });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
 
-	res.setHeader('Allow', ['DELETE']);
-	res.status(405).end(`Method ${req.method} is not allowed.`);
+  res.setHeader("Allow", ["DELETE"]);
+  res.status(405).end(`Method ${req.method} is not allowed.`);
 };
-
 
 export default handler;
