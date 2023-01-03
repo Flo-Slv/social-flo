55ef4ee83790366c1b94370e29a4675fd6d1a5b2
Flo Slv <florent3969@hotmail.fr>
2023-01-03 10:48:06 +0100

    🟦 feat: handle error display with taillwindcss

--- app/(backend)/profile/error.jsx
+++ app/(backend)/profile/error.jsx
@@ -4,7 +4,7 @@ const Error = ({ error, reset }) => {
   console.log("client error profile: ", error);
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center h-screen text-red-500">
       <p>Something went wrong !</p>
       <button onClick={() => reset()}>Reset error boundary</button>
     </div>
