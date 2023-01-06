8cae19cbe66ee891cba0282ee8ced041e7f192c5
Flo Slv <florent3969@hotmail.fr>
2023-01-06 14:36:47 +0100

    🟪 refactor: add comment when set Prisma client

--- prisma/utils/prismaClient.js
+++ prisma/utils/prismaClient.js
@@ -1,16 +1,23 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
-if (process.env.NODE_ENV === 'production')
-	prisma = new PrismaClient();
+if (process.env.NODE_ENV === "production") prisma = new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') {
-	if (!global.prisma) {
-		global.prisma = new PrismaClient();
-	}
+if (process.env.NODE_ENV === "development") {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
 
-	prisma = global.prisma;
+  prisma = global.prisma;
 }
 
 export default prisma;
+
+// ANOTHER WAY TO HANDLE !
+// import { PrismaClient } from "@prisma/client";
+//
+// const client = globalThis.prisma || new PrismaClient();
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+//
+// export default client;
